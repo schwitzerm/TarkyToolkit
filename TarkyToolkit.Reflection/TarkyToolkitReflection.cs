4d17cc3c24dae1c7bb361d46620716337d256034
Mitchell Schwitzer <mitchell@schwitzer.ca>
2025-04-27 17:28:43 -0700

    pedantry...

--- TarkyToolkit.Reflection/TarkyToolkitReflection.cs
+++ TarkyToolkit.Reflection/TarkyToolkitReflection.cs
@@ -3,7 +3,7 @@
 namespace TarkyToolkit.Reflection;
 
 /// <summary>
-/// Stub to load plugin. Needed to load and require this plugin as a dependency in BepInEx.
+/// Stub to load binary. Needed to load and require this binary as a dependency in BepInEx.
 /// </summary>
 [BepInPlugin("Mellow_.TarkyToolkit.Reflection", "TarkyToolkit.Reflection", "0.1.0")]
 public class TarkyToolkitReflection : BaseUnityPlugin
--- TarkyToolkit.Shared/TarkyToolkitShared.cs
+++ TarkyToolkit.Shared/TarkyToolkitShared.cs
@@ -3,7 +3,7 @@
 namespace TarkyToolkit.Shared;
 
 /// <summary>
-/// Stub to load plugin. Needed to load and require this plugin as a dependency in BepInEx.
+/// Stub to load binary. Needed to load and require this binary as a dependency in BepInEx.
 /// </summary>
 [BepInPlugin("Mellow_.TarkyToolkit.Shared", "TarkyToolkit.Shared", "0.1.0")]
 public class TarkyToolkitShared : BaseUnityPlugin
