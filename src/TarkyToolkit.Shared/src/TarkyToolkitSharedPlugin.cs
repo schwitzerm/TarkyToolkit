8a56b5a0781e154ea46f2ac63ccf16e9d60e9823
Mitchell Schwitzer <mitchell@schwitzer.ca>
2025-04-29 01:37:19 -0700

    project structuring..

---
+++ src/TarkyToolkit.Shared/src/TarkyToolkitSharedPlugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using JetBrains.Annotations;
+using TarkyToolkit.Core.Logging;
+
+namespace TarkyToolkit.Shared
+{
+    /// <summary>
+    /// Stub to load binary. Needed to load and require this binary as a dependency in BepInEx.
+    /// </summary>
+    [BepInPlugin("Mellow_.TarkyToolkit.Shared", "TarkyToolkit.Shared", "0.1.0")]
+    public class TarkyToolkitSharedPlugin : BaseUnityPlugin
+    {
+        internal new static Logger Logger { get; private set; } = null!;
+
+        [UsedImplicitly]
+        private void Awake()
+        {
+            Logger = new BepLogger(base.Logger);
+        }
+    }
+}
